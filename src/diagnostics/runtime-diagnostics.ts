0d64d9469eba02a6cf9de2965c8a26c76fac5c7f
zsumz <shawn@zsumz.com>
2026-06-24 14:07:12 -0500

    feat(diagnostics): add runtime diagnostics

---
+++ src/diagnostics/runtime-diagnostics.ts
@@ -0,0 +1,17 @@
+export {
+    DiagnosticDatabaseConnection,
+} from './runtime/diagnostic-database-connection';
+export type {
+    IncludeDiagnosticEvent,
+    LazyLoadDiagnosticEvent,
+    MigrationDiagnosticEvent,
+    QueryDiagnosticEvent,
+    QueryPlanDiagnosticEvent,
+    QueryPlanShape,
+    DiagnosticsOptions,
+    RuntimeDiagnosticEvent,
+    RuntimeDiagnosticsHandler,
+    SaveChangesDiagnosticEvent,
+    SaveChangesDiagnosticPlanEntry,
+    TransactionDiagnosticEvent,
+} from './runtime/events';
