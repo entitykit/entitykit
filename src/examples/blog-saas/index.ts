b06f834d9812dceb5189a0753b25cbdb385f445e
zsumz <shawn@zsumz.com>
2026-07-29 12:48:18 -0500

    feat(examples): add public surface

---
+++ src/examples/blog-saas/index.ts
@@ -0,0 +1,9 @@
+export { BlogSaasDbContext } from './blog-saas-db-context';
+export { BlogPost } from './blog-post';
+export { BlogUser } from './blog-user';
+export { Comment } from './comment';
+export { Membership } from './membership';
+export type { MembershipRole } from './membership';
+export { Tag } from './tag';
+export { Workspace } from './workspace';
+export { runBlogSaasExample } from './example';
---
+++ src/examples/crud-app/index.ts
@@ -0,0 +1,8 @@
+export { AppUser } from './app-user';
+export { CrudAppDbContext } from './crud-app-db-context';
+export { Project } from './project';
+export { TaskComment } from './task-comment';
+export { TaskItem } from './task-item';
+export type { TaskStatus } from './task-item';
+export { Workspace } from './workspace';
+export { runCrudAppExample } from './example';
