8da981be63b5b6decfb71e6bdfb05310c56cc55b
zsumz <shawn@zsumz.com>
2026-07-29 17:02:45 -0500

    test(support): cover provider contract

---
+++ tests/support/provider-contract.ts
@@ -0,0 +1,5 @@
+export { contractMigrationHistoryRow, expectDatabaseProviderError } from './provider-contract/assertions';
+export { defineProviderContractTests } from './provider-contract/define-provider-contract-tests';
+export { CreateProviderContractUsers, ExerciseProviderMigrationOperations } from './provider-contract/migrations';
+export { ProviderContractChild, ProviderContractDbContext, ProviderContractParent, ProviderContractUser, ProviderContractValue } from './provider-contract/model';
+export type { ProviderContractRuntime, ProviderContractRuntimeFactory } from './provider-contract/runtime';
